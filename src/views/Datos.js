db9ee47aedecbdbadf9f746b39e384267809bf9e
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2020-10-27 12:49:16 -0600

    Agrega prototipo de vista.

--- src/views/Datos.js
+++ src/views/Datos.js
@@ -1,4 +1,3 @@
-import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import React from 'react';
 
--- src/views/Grafica.js
+++ src/views/Grafica.js
@@ -1,4 +1,3 @@
-import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import React from 'react';
 import {
@@ -10,59 +9,32 @@ import {
   Tooltip,
   Legend
 } from 'recharts';
-
-const data = [
-  {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100
-  }
-];
+import { StateContext } from 'state/StateProvider';
 
 const Basic = (props) => {
+  const context = React.useContext(StateContext);
+  const state = context.state;
+  const data = state.data;
+
+  const initialState = {
+    data,
+    left: 'dataMin',
+    right: 'dataMax',
+    refAreaLeft: '',
+    refAreaRight: '',
+    top: 'dataMax+1',
+    bottom: 'dataMin-1',
+    top2: 'dataMax+20',
+    bottom2: 'dataMin-20',
+    animation: true
+  };
+
   return (
     <div>
       <LineChart
         width={900}
         height={600}
-        data={data}
+        data={state.data}
         margin={{
           top: 5,
           right: 30,
@@ -70,17 +42,29 @@ const Basic = (props) => {
           bottom: 5
         }}>
         <CartesianGrid strokeDasharray='3 3' />
-        <XAxis dataKey='name' />
+        <XAxis dataKey='name' padding={{ left: 50, right: 50 }} />
         <YAxis />
         <Tooltip />
         <Legend />
-        <Line
-          type='monotone'
-          dataKey='pv'
-          stroke='#8884d8'
-          activeDot={{ r: 8 }}
-        />
-        <Line type='monotone' dataKey='uv' stroke='#82ca9d' />
+
+        {console.log('algs', state.algs)}
+        {state.algs?.map((alg) => (
+          <Line
+            key={alg}
+            type='monotone'
+            dataKey={alg}
+            stroke={state.colors[alg]}
+          />
+        ))}
+
+        {/* <Line */}
+        {/*   type='monotone' */}
+        {/*   dataKey='CHASELEV' */}
+        {/*   stroke='#8884d8' */}
+        {/*   activeDot={{ r: 8 }} */}
+        {/* /> */}
+        {/* <Line type='monotone' dataKey='uv' stroke='#82ca' /> */}
+        {/* <Line type='monotone' dataKey='amt' stroke='#82ca9d' /> */}
       </LineChart>
     </div>
   );
