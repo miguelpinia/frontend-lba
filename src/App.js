ea50b569f3f296c7cefdcc642b0d60270fd12653
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2020-10-27 18:44:31 -0600

    Elimina mocks.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@ import React from 'react';
 import { Route, Router, Switch } from 'react-router-dom';
 import Main from 'views/Main';
 import { createBrowserHistory } from 'history';
-import mock from './utils/mock';
 
 const history = createBrowserHistory();
 
 const App = () => {
-  mock();
   return (
     <Router history={history}>
       <Switch>
